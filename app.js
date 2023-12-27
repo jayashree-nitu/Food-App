c3aa624aea9e70ae24ce3f2b25cfee1409ad9ef1
Jayashree Mohanty <82712562+jayashree-nitu@users.noreply.github.com>
2023-12-27 11:27:38 +0530

    episode-03

--- app.js
+++ app.js
@@ -1,31 +1,34 @@
 import React from "react";
-import  ReactDOM  from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
-/*<div id="parent">
- *      <div id="child1">
- *         <h1>I'm h1 tag</h1>
- *         <h2>I'm h1 tag</h2>
- *      </div>
- * <div id="child2">
- *         <h1>I'm h1 tag</h1>
- *         <h2>I'm h1 tag</h2>
- *      </div>
- * </div>
- *
- * ReactElement(Object) => HTML(Browser Understands)
- */
-const parent  
-   =React.createElement("div",{id:"parent"},[
-    React.createElement("div",{id:"child1"},[
-    React.createElement("h1",{},"I'm h1 tag"),
-    React.createElement("h2",{},"I'm h2 tag")
-]),
-    React.createElement("div",{id:"child2"},[
-    React.createElement("h1",{},"I'm h1 tag"),
-    React.createElement("h2",{},"I'm h2 tag")
-]),
-   ]);
- console.log(parent); // object
 
-const root =ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+const heading = React.createElement("h1", { id: "heading" }, "Namaste 🚀");
+//jsx = HTML like or XML like syntax
+//jsx = (transpiled before it reaches the JS Engine)-Parcel-babel
+
+//React Element
+const Jsx = (
+  <h1 className="head" tabIndex="1">
+    Namaste React using 
+  </h1>
+);
+const Title=()=> (
+  <h1 className="head" tabIndex="1">
+    Namaste React using Jsx
+  </h1>
+);
+
+// React Fragment - behaves like an empty tag
+const HeadingComponent = () => (
+  <div id="container">
+    {Title()}
+    {Jsx}
+    <h1 className="heading">Namaste React FunctionalComponent</h1>
+  </div>
+);
+
+//console.log(JSXHeading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent/>);
